6f12fd4cb1da8510e8c38403e8a3e788b93d00bb
iwrotesomecode <writedumont@gmail.com>
2020-12-24 17:54:17 -0800

    Ex. 2.1 handle positive and negative rationals

---
+++ 2.1.rkt
@@ -0,0 +1,59 @@
+#lang sicp
+
+#| Exercise 2.1: Define a better version of make-rat that handles both positive
+and negative arguments. Make-rat should normalize the sign so that if the
+rational number is positive, both the numerator and denominator are positive,
+and if the rational number is negative, only the numerator is negative.
+|#
+
+(define (make-rat n d)
+  ; check if denominator is negative
+  (if (< d 0)
+      ; make sure negatives expressed in numerator only 
+      (make-rat (* -1 n) (* -1 d))
+      (let ((g (gcd n d)))
+        (cons (/ n g) (/ d g)))))
+
+; Euclid's Algorithm 
+(define (gcd a b)
+  (if (= b 0)
+      (abs a)
+      (gcd (abs b) (remainder (abs a) (abs b)))))
+
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+
+(print-rat (make-rat -3 6))
+(print-rat (make-rat 3 -6))
+(print-rat (make-rat -3 -6))
+(print-rat (mul-rat (make-rat 2 -3) (make-rat 1 4))) ;-1/6
+(print-rat (div-rat (make-rat -1 -2) (make-rat -3 7)))  ;-7/6
