a01d65f34ca5d49222e913f4ee79f65cd0aaad80
iwrotesomecode <writedumont@gmail.com>
2020-12-30 22:32:55 -0800

    Evaluation of different list joining methods

---
+++ 2.26.rkt
@@ -0,0 +1,23 @@
+#lang sicp
+
+#| Exercise 2.26: Suppose we define x and y to be two lists:
+
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+What result is printed by the interpreter in response to evaluating each of the following expressions:
+
+(append x y)
+(cons x y)
+(list x y)
+
+|#
+
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+
+(display (append x y)) ; (1 2 3 4 5 6)
+(newline)
+(display (cons x y)) ; ((1 2 3) (4 5 6))
+(newline)
+(display (list x y)) ; ((1 2 3) (4 5 6))
